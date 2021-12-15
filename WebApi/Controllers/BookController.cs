8ab6663800ecfcf323ad2592dc65eb1e582fd82c
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-15 14:06:24 +0300

    Add ef core and Configure BookStoreDbContext

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -11,27 +11,7 @@ namespace WebApi.AddControllers
     {
         private static List<Book> BookList = new List<Book>()
         {
-            new Book {
-                Id = 1,
-                Title = "Lean Startup",
-                GenreId = 1,
-                PageCount = 200,
-                PublishDate = new DateTime(2001,02,06)
-            },
-            new Book {
-                Id = 2,
-                Title = "Herland",
-                GenreId = 2,
-                PageCount = 250,
-                PublishDate = new DateTime(2010,03,12)
-            },
-            new Book {
-                Id = 3,
-                Title = "Dune",
-                GenreId = 2,
-                PageCount = 250,
-                PublishDate = new DateTime(2011,12,12)
-            }
+
         };
 
         [HttpGet]
---
+++ WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+
+                context.Books.AddRange(
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Lean Startup",
+                        GenreId = 1,
+                        PageCount = 200,
+                        PublishDate = new DateTime(2001, 02, 06)
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "Herland",
+                        GenreId = 2,
+                        PageCount = 250,
+                        PublishDate = new DateTime(2010, 03, 12)
+                    },
+                    new Book
+                    {
+                        Id = 3,
+                        Title = "Dune",
+                        GenreId = 2,
+                        PageCount = 250,
+                        PublishDate = new DateTime(2011, 12, 12)
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
