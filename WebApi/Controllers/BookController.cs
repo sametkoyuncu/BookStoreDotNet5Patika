8b4e83fbe9a1bcb8fc79c97ec624c70f1649ad48
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-16 19:49:41 +0300

    End of 'DB Context kullanarak CRUD işlemleri'

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
 
 namespace WebApi.AddControllers
 {
@@ -9,15 +10,17 @@ namespace WebApi.AddControllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-        private static List<Book> BookList = new List<Book>()
-        {
+        private readonly BookStoreDbContext _context;
 
-        };
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public List<Book> GetBooks()
         {
-            var booklist = BookList.OrderBy(x => x.Id).ToList<Book>();
+            var booklist = _context.Books.OrderBy(x => x.Id).ToList<Book>();
             return booklist;
         }
 
@@ -25,32 +28,33 @@ namespace WebApi.AddControllers
         [Route("{id}")]
         public Book GetBookById(int id)
         {
-            var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+            var book = _context.Books.Where(x => x.Id == id).SingleOrDefault();
             return book;
         }
 
         // [HttpGet]
         // public Book GetBookByIdFromQuery([FromQuery] string id)
         // {
-        //     var book = BookList.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //     var book = _context.Books.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
         //     return book;
         // }
 
         [HttpPost]
         public IActionResult AddBook([FromBody] Book newBook)
         {
-            var book = BookList.SingleOrDefault(b => b.Title == newBook.Title);
+            var book = _context.Books.SingleOrDefault(b => b.Title == newBook.Title);
             if (book is not null)
                 return BadRequest();
 
-            BookList.Add(newBook);
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
             return Ok();
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
         {
-            var book = BookList.SingleOrDefault(b => b.Id == updatedBook.Id);
+            var book = _context.Books.SingleOrDefault(b => b.Id == updatedBook.Id);
             if (book is null)
                 return BadRequest();
             // gelen veri default değer mi? yani boş mu gelmiş?
@@ -60,17 +64,19 @@ namespace WebApi.AddControllers
             book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
             book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
 
+            _context.SaveChanges();
             return Ok();
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = BookList.SingleOrDefault(b => b.Id == id);
+            var book = _context.Books.SingleOrDefault(b => b.Id == id);
             if (book is null)
                 return BadRequest();
 
-            BookList.Remove(book);
+            _context.Books.Remove(book);
+            _context.SaveChanges();
             return Ok();
         }
     }
