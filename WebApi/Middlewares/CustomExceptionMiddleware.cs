79d48f454d6aaa9de7af1213183aa0bc9a4f511f
sametkoyuncu <sametkoyuncu@live.com>
2022-01-10 21:38:14 +0300

    End of 'DI Container Kullanark Logger Srvs Eklemk'

--- WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using WebApi.Services;
 
 namespace WebApi.Middlewares
 {
     public class CustomExceptionMiddleware
     {
         private readonly RequestDelegate _next;
-        public CustomExceptionMiddleware(RequestDelegate next)
+        private readonly ILoggerService _loggerService;
+        public CustomExceptionMiddleware(RequestDelegate next, ILoggerService loggerService)
         {
             _next = next;
+            _loggerService = loggerService;
         }
 
         public async Task Invoke(HttpContext context)
@@ -23,13 +26,13 @@ namespace WebApi.Middlewares
             {
 
                 string message = "[Request]  HTTP " + context.Request.Method + " - " + context.Request.Path;
-                Console.WriteLine(message);
+                _loggerService.Write(message);
 
                 await _next(context);
                 watch.Stop();
 
                 message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responsed " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + "ms";
-                Console.WriteLine(message);
+                _loggerService.Write(message);
             }
             catch (Exception ex)
             {
@@ -45,7 +48,7 @@ namespace WebApi.Middlewares
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "[Error]  HTPP " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + "ms";
-            Console.WriteLine(message);
+            _loggerService.Write(message);
 
 
 
