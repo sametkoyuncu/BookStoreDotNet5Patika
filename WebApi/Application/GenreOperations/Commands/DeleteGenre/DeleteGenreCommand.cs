58fb2130c3e68882532ff059b43daf90caad4620
sametkoyuncu <sametkoyuncu@live.com>
2022-01-17 22:16:49 +0300

    Add delete genre commands

---
+++ WebApi/Application/GenreOperations/Commands/DeleteGenre/DeleteGenreCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using WebApi.DBOperations;
+
+namespace WebApi.Application.GenreOprerations.Commands.DeleteGenre
+{
+    public class DeleteGenreCommand
+    {
+        public int Id { get; set; }
+        private readonly BookStoreDbContext _context;
+        public DeleteGenreCommand(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle()
+        {
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == Id);
+
+            if (genre is null)
+                throw new InvalidOperationException("Kitap Türü Bulunamadı!");
+
+            _context.Genres.Remove(genre);
+            _context.SaveChanges();
+        }
+    }
+}
