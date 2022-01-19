84cd5eed85c5c8c92573ff91851125925224e360
sametkoyuncu <sametkoyuncu@live.com>
2022-01-19 15:04:38 +0300

    Add update author command

---
+++ WebApi/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommand.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using WebApi.DBOperations;
+
+namespace WebApi.Application.AuthorOperations.Commands.UpdateAuthor
+{
+    public class UpdateAuthorCommand
+    {
+        private readonly BookStoreDbContext _context;
+        public int Id { get; set; }
+        public UpdateAuthorModel Model = new UpdateAuthorModel();
+
+        public UpdateAuthorCommand(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x => x.Id == Id);
+
+            if (author is null)
+                throw new InvalidOperationException("Yazar bulunamadı");
+
+            author.FirstName = string.IsNullOrEmpty(Model.FirstName.Trim()) ? author.FirstName : Model.FirstName;
+
+            author.LastName = string.IsNullOrEmpty(Model.LastName.Trim()) ? author.LastName : Model.LastName;
+
+            _context.SaveChanges();
+        }
+    }
+
+    public class UpdateAuthorModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ WebApi/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommandValiadtor.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+namespace WebApi.Application.AuthorOperations.Commands.UpdateAuthor
+{
+    public class UpdateAuthorCommandValidator : AbstractValidator<UpdateAuthorCommand>
+    {
+        public UpdateAuthorCommandValidator()
+        {
+
+            RuleFor(command => command.Id).GreaterThan(0);
+            RuleFor(command => command.Model.FirstName).NotEmpty().MinimumLength(2);
+            RuleFor(command => command.Model.LastName).NotEmpty().MinimumLength(2);
+        }
+    }
+}
